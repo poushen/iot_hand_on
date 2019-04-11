d6cb1babc52a0773d24738a19d2780a9216a2ad6
Phou-shen Ou <31812245+poushen@users.noreply.github.com>
2019-04-11 19:22:25 +0800

    add cp command to my.py

--- lesson_4/my.py
+++ lesson_4/my.py
@@ -32,3 +32,25 @@ def ip():
     wlan = network.WLAN(network.STA_IF)
 
     print('ip: {}'.format(wlan.ifconfig()[0]))
+    
+def fileExists(path):
+    file_size = 0
+    try:
+        s = os.stat(path)
+        if s[0] != 0x4000:
+            file_size = s[6]
+        return file_size
+    except OSError as e:
+        if (e.args[0] == 2):
+            print('No such file!')
+            return None
+        
+def cp(src, dest):
+    if fileExists(src):
+        with open(src, 'rb') as f_src:
+            with open(dest, 'wb') as f_dest:
+                while True:
+                    chunk = f_src.read(64)
+                    if len(chunk) == 0:
+                        break
+                    f_dest.write(chunk)
