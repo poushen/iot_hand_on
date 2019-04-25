75d713883ba8b9bab0f836cb739e8888638bb807
Poushen <poushen>
2019-04-25 20:36:11 +0800

    update ads1115_ohm.py, print Vout and I

--- lesson_08/ads1115_ohm.py
+++ lesson_08/ads1115_ohm.py
@@ -9,7 +9,11 @@ ads = ads1x15.ADS1115(i2c, addr, gain)
 
 r2 = float(input('R2? '))
 vcc = ads.raw_to_v(ads.read(channel1=1))
+vout = ads.raw_to_v(ads.read())
+I = vout / r2
 
-r1 = r2 * (vcc / ads.raw_to_v(ads.read()) - 1)
+r1 = r2 * (vcc / vout - 1)
 
 print('R1 = {:.2f} ohm'.format(r1))
+print('Vout = {:.2f} V'.format(vout))
+print('I = {:.4f} A'.format(I))
