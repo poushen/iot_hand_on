ad23ed2966b15198f03d9092a6d0eef1f6dcfe7a
Poushen <poushen>
2019-04-30 19:03:15 +0800

    create publish_am2320_aio.py

---
+++ lesson_15/publish_am2320_aio.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+   程式說明請參閱18-22頁
+   publish am2320's temperature & humidity to Adafruit IO
+"""
+
+import machine
+import ubinascii
+import time
+import am2320
+from machine import I2C, Pin
+from umqtt.simple import MQTTClient
+
+i2c = I2C(scl=Pin(5), sda=Pin(4))
+d = am2320.AM2320(i2c)
+
+config = {
+    'broker':'io.adafruit.com',
+    'user':'poushen',
+    'key':'68c3991e7a7a49b6bba4a83ca8110aff',
+    'id' : 'room/' + ubinascii.hexlify(machine.unique_id()).decode(),
+    'topic_temp' : b'poushen/feeds/temperature',
+    'topic_humi' : b'poushen/feeds/humidity'
+}
+
+def publish(topic, data):
+    client = MQTTClient(client_id=config['id'],
+                    server=config['broker'],
+                    user=config['user'],
+                    password=config['key'])
+
+    client.connect()
+    client.publish(topic, data.encode())
+    time.sleep(1)
+    client.disconnect()
+    
+def main():
+    while True:
+        d.measure()
+        data = '{}'.format(d.temperature())
+        publish(config['topic_temp'], data)
+        time.sleep(1)
+        data = '{}'.format(d.humidity())
+        publish(config['topic_humi'], data)
+        time.sleep(8)
+        
+try:
+    main()
+except KeyboardInterrupt:
+    print('bye')
+except Exception as ex:
+    print(ex)
