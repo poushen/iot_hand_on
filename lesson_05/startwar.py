fe3e6356285d327c920bfa4fc23ad3742e6eb030
poushen <31812245+poushen@users.noreply.github.com>
2019-05-17 11:09:41 +0800

    fix star war typo error

--- lesson_05/startwar.py
+++
@@ -1,9 +0,0 @@
-import socket
-addr_info = socket.getaddrinfo("towel.blinkenlights.nl", 23)
-addr = addr_info[0][-1]
-s = socket.socket()
-s.connect(addr)
-while True:
-    data = s.recv(500)
-    print(str(data, 'utf8'), end='')
-    
